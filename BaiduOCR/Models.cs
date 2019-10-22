e29c93b8d4629110a3c503828a66b6c25ff3d0dd
ken liang <349834403@qq.com>
2019-10-22 11:27:26 +0800

    Create Models.cs

---
+++ BaiduOCR/Models.cs
@@ -0,0 +1,43 @@
+﻿namespace BaiduOCR
+{
+    public class Models
+    {
+        public string MajorName { get; set; }
+        public string MajorNum { get; set; }
+        public string CollegeName { get; set; }
+        public string CollegeNum { get; set; }
+
+        public Models(string majorName, string majorNum, string collegeName, string collegeNum)
+        {
+            MajorName = majorName;
+            MajorNum = majorNum;
+            CollegeName = collegeName;
+            CollegeNum = collegeNum;
+        }
+    }
+
+    public class ZSJH
+    {
+        public string MajorName { get; set; }
+        public string CollegeNum { get; set; }
+        public string CollegeName { get; set; }
+        public string Persons { get; set; }
+        public string XueNian { get; set; }
+        public string Fee { get; set; }
+
+        public ZSJH(string majorName, string collegeNum, string collegeName, string persons,string xueNian,string fee)
+        {
+            MajorName = majorName;
+            CollegeNum = collegeNum;
+            CollegeName = collegeName;
+            Persons = persons;
+            XueNian = xueNian;
+            Fee = fee;
+        }
+
+        public ZSJH()
+        {
+            
+        }
+    }
+}
